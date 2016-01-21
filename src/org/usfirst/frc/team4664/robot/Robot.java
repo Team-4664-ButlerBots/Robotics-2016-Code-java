5490169cdfa75da5a3ec88f876af1f8e2e7851c1
JWCS <sauve031@umn.edu>
2016-01-21 16:47:51 -0600

    controller support

--- src/org/usfirst/frc/team4664/robot/Robot.java
+++ src/org/usfirst/frc/team4664/robot/Robot.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive class.
@@ -13,24 +14,41 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends SampleRobot {
 	
     RobotDrive robotDrive;
-    Joystick stick;
+    Victor armMotor;
+    Victor clawMotor;
+    
+	double scaleArm= -0.7, scaleClaw= -1.0, scaleJoy= 0.6;
+	double dbdArm= 0.2, dbdClaw= 0.3, dbdJoy= 0.3;
+    
+    Joystick stickDrive;
+    Joystick stickArm;
 
     // Channels for the wheels
     final int frontLeftChannel	= 2;
     final int rearLeftChannel	= 3;
-    final int frontRightChannel	= 1;
-    final int rearRightChannel	= 0;
+    final int frontRightChannel	= 0;
+    final int rearRightChannel	= 1;
+    
+    final int armLift = 4;
+    final int armBite = 5;
     
     // The channel on the driver station that the joystick is connected to
-    final int joystickChannel	= 0;
+    final int joystickChannel1	= 0;
+    final int joystickChannel2  = 1;
 
     public Robot() {
         robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
-    	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
-    	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);		// you may need to change or remove this to match your robot
+        armMotor = new Victor(armLift);
+        clawMotor = new Victor(armBite);
+        
+        robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
+        robotDrive.setInvertedMotor(MotorType.kRearRight, true);
+    	//robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
+    	//robotDrive.setInvertedMotor(MotorType.kRearLeft, true);   // you may need to change or remove this to match your robot
         robotDrive.setExpiration(0.1);
 
-        stick = new Joystick(joystickChannel);
+        stickDrive = new Joystick(joystickChannel1);
+        stickArm   = new Joystick(joystickChannel2);
     }
         
 
@@ -43,10 +61,28 @@ public class Robot extends SampleRobot {
         	
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-            robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), stick.getZ(), 0);
+            robotDrive.mecanumDrive_Cartesian(scaleJoy*ApplyDeadband(stickDrive.getX(), dbdJoy),
+            						  scaleJoy*ApplyDeadband(stickDrive.getY(), dbdJoy), 0, 0);
+            //robotDrive.arcadeDrive(stick); 
+            
+            armMotor.set(ApplyDeadband(stickArm.getX(), dbdJoy));
+            clawMotor.set(ApplyDeadband(stickArm.getY(), dbdJoy));
             
             Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
         }
     }
-    
+
+	double ApplyDeadband(double rawValue, double deadband) {
+		rawValue = Limit(rawValue);
+		if(Math.abs(rawValue) < deadband) return 0.0;
+		if(rawValue > 0)			      return (rawValue - deadband) / (1.0 - deadband);
+									      return (rawValue + deadband) / (1.0 - deadband);
+	}
+	
+	double Limit(double value) {
+		if(value > 1.0)  return 1.0;
+		if(value < -1.0) return -1.0;
+						 return value;
+	}
+	
 }
