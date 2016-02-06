93371f60000f009ce8861c2c0c9f0dc48fcc55b3
Jasonface900 <jorge.floresm900@gmail.com>
2016-02-06 12:07:23 -0600

    tank drive first commit

--- src/org/usfirst/frc/team4664/robot/Robot.java
+++ src/org/usfirst/frc/team4664/robot/Robot.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team4664.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
@@ -11,106 +10,50 @@ import edu.wpi.first.wpilibj.Victor;
  //This is a demo program showing how to use Mecanum control with the RobotDrive class.
  
 public class Robot extends SampleRobot {
-	
-    RobotDrive RobotDrive;
+	//Systems
+    RobotDrive TankDrive;
     Joystick StickController;
-    Joystick Jstick;
-    //DigitalInput LimitSwitchArm;
-    //DigitalInput LimitSwitchClaw;
+    //Motors
+    Victor rightSide;
+    Victor leftSide;
+    Victor arm;
+    Victor capture;
     
-     //Channels for the wheels
-    final int frontLeftChannel	= 2;
-    final int rearLeftChannel	= 3;
-    final int frontRightChannel	= 1;
-    final int rearRightChannel	= 0;
     
-    Victor armLift;
-    Victor clawTote;
-    
-    boolean speedOverride = false;
+    final int lsMotor	= 0;
+    final int rsMotor	= 1;
+    final int armPort   = 2;
+    final int capturePort=3;
     
     //The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
-    final int joystickChannel1 	= 1;
 
     public Robot() {
-        RobotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
-    	//RobotDrive.setInvertedMotor(MotorType.kFrontRight, true);	 //invert the left side motors
-    	//RobotDrive.setInvertedMotor(MotorType.kRearRight, true);		 //you may need to change or remove this to match your robot
-        RobotDrive.setExpiration(0.1);
-
+    	rightSide = new Victor(lsMotor);
+    	leftSide  = new Victor(rsMotor);
+    	arm       = new Victor(armPort);
+    	capture   = new Victor(capturePort);
+    	
+        TankDrive = new RobotDrive(leftSide, rightSide);
+    	
         StickController = new Joystick(joystickChannel);
-        Jstick = new Joystick(joystickChannel1);
-        
-        armLift = new Victor(4);
-        clawTote = new Victor(5);
-        //LimitSwitchArm = new DigitalInput(8);
-        //LimitSwitchClaw = new DigitalInput(9);
     	}
     
-      //Runs the motors with mecanum drive.
-    public void autonomousPeriodic() {
-    	RobotDrive.mecanumDrive_Cartesian(.25,0,0,0);
-    	Timer.delay(1.5);
-    	RobotDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
-    	armLift.set(-0.5);
-    	Timer.delay(2.0);
-    	armLift.set(0);
-    }
-    
     public void operatorControl() {
-        RobotDrive.setSafetyEnabled(true);
+        TankDrive.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
+        	TankDrive.tankDrive(DeadBand(StickController.getY()), DeadBand(StickController.getRawAxis(3)));
+        	if(StickController.getRawButton(6)) {
+        		arm.set(0.5);
+        	}
+        	else if(StickController.getRawButton(5)) {
+        		arm.set(-0.5);
+        	}
+        	else {
+        		arm.set(0);
+        	}
         	
-        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
-        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-            if(speedOverride = true) {
-            	RobotDrive.mecanumDrive_Cartesian(DeadBand(StickController.getX()), DeadBand(StickController.getZ()), DeadBand(-StickController.getY()), 0);
-                if(StickController.getRawButton(4)) {
-            		speedOverride = false;
-                	Timer.delay(.0625);
-            	}
-            	if(StickController.getRawButton(7)) {
-                	armLift.set(-.9);
-            	}
-            	else if(StickController.getRawButton(8)) {
-            		armLift.set(.8);
-            	}
-            	else if(StickController.getRawButton(6)) {
-                	clawTote.set(.8);
-            	}
-                else if(StickController.getRawButton(5)) {
-                	clawTote.set(-.9);
-            	}
-                else {
-            		armLift.set(0);
-            		clawTote.set(0);
-                }  
-            }
-            else {
-            	RobotDrive.mecanumDrive_Cartesian(DeadBand(StickController.getX()*.5), DeadBand(StickController.getZ()*.5), DeadBand(StickController.getY()*.5), 0);
-            	if(StickController.getRawButton(4)) {
-            		speedOverride = true;
-            		Timer.delay(.0625);
-            	}
-            	if(StickController.getRawButton(7)) {
-            		armLift.set(-.3);
-            	}
-            	else if(StickController.getRawButton(8)) {
-            		armLift.set(.2);
-            	}
-            	else if(StickController.getRawButton(6)) {
-            		clawTote.set(.2);
-            	}
-            	else if(StickController.getRawButton(5)) {
-            		clawTote.set(-.3);
-            	}
-            	else {
-            		armLift.set(0);
-            		clawTote.set(0);
-            	}
-            Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
-            }
+        	Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles   
         }
     }
     //Useful functions
