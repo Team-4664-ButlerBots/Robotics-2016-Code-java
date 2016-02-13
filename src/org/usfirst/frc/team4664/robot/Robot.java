d10e084d45a1bdee4f3f473b5b70676430fd017c
Jude Sauve <sauve031@umn.edu>
2016-02-13 13:31:07 -0600

    finalized barebones code

--- src/org/usfirst/frc/team4664/robot/Robot.java
+++ src/org/usfirst/frc/team4664/robot/Robot.java
@@ -6,57 +6,98 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor; 
 public class Robot extends SampleRobot {
 	//Systems
-    RobotDrive TankDrive;
-    Joystick StickController;
+    RobotDrive driveTrain;
+    Joystick joy1, joy2;
     //Motors
-    Victor rightSide;
-    Victor leftSide;
-    Victor arm;
-    Victor capture;
-    
+    Victor rightSide, leftSide;//Drive train motors
+    Victor lattice, winch;//The Scissor lift & winch respectively
+    Victor armSpeed, armTorque;//armSpeed spins the intake wheels; armTorque moves input in out
+    //Ports
     final int lsMotor	  = 0;
     final int rsMotor	  = 1;
-    final int armPort     = 2;
-    final int capturePort = 3;
+    final int armTPort    = 2;
+    final int armSPort	  = 3;
+    final int latPort     = 4;
+    final int winchPort   = 5;
+    //joystick 2 buttons
+    final int armSpeedB    = 6;
+    final int latticeUpB   = 3;
+    final int latticeDownB = 2;
+    final int winchOutB    = 4;
+    final int winchInB     = 5;
+    //speed variables
+    final double armSpeedVal   = 0.25;
+    final double winchOut      = 1.0;
+    final double winchIn       = -.7;
+    final double latticeUp     = 0.8;
+    final double latticeDown   = -.5;
+    //dead band variables
+    final double driveXDb    = 0.3;
+    final double driveYDb    = 0.3;
+    final double armTorqueDb = 0.2;
+    //Laptop ports
+    final int joy1Port	= 0;
+    final int joy2Port  = 1;
     
-    //The channel on the driver station that the joystick is connected to
-    final int joystickChannel	= 0;
-
     public Robot() {
     	rightSide = new Victor(rsMotor);
     	leftSide  = new Victor(lsMotor);
-    	arm       = new Victor(armPort);
-    	capture   = new Victor(capturePort);
-    	
-        TankDrive = new RobotDrive(leftSide, rightSide);
-    	
-        StickController = new Joystick(joystickChannel);
+    	armSpeed  = new Victor(armSPort);
+    	armTorque = new Victor(armTPort);
+    	lattice   = new Victor(latPort);
+    	winch     = new Victor(winchPort);
+        driveTrain = new RobotDrive(leftSide, rightSide);
+        joy1 = new Joystick(joy1Port);
+        joy2 = new Joystick(joy2Port);
     	}
     
     public void operatorControl() {
-        TankDrive.setSafetyEnabled(true);
+        driveTrain.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-        	TankDrive.tankDrive(DeadBand(-StickController.getY()), DeadBand(-StickController.getRawAxis(3)));
-        	if(StickController.getRawButton(6)) {
-        		capture.set(1.0);
+        	//Drivetrain
+        	driveTrain.arcadeDrive(Deadband(joy1.getX(), driveXDb), Deadband(joy1.getY(), driveYDb)); //joy1 is drive
+        	//Arm Code
+        	armTorque.set(Deadband(joy2.getY(), armTorqueDb));  //armTorque
+        	if(joy2.getRawButton(armSpeedB)){					//armSpeed
+        		armSpeed.set(armSpeedVal);
+        	}else{
+        		armSpeed.set(0.0);
         	}
-        	else if(StickController.getRawButton(5)) {
-        		capture.set(-1.0);
+        	//lift system code
+        	if(joy2.getRawButton(latticeUpB)){					//lattice
+        		lattice.set(latticeUp);
+        	}else if(joy2.getRawButton(latticeDownB)){
+        		lattice.set(latticeDown);
+        	}else{
+        		lattice.set(0.0);
         	}
-        	else {
-        		capture.set(0);
+        	if(joy2.getRawButton(winchOutB)){					//winch
+        		winch.set(winchOut);
+        	}else if(joy2.getRawButton(winchInB)){
+        		winch.set(winchIn);
+        	}else{
+        		winch.set(0.0);
         	}
-        	
         	Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles   
         }
     }
-    //Useful functions
-        double DeadBand(double input) {
-        	if(Math.abs(input) < 0.1) {
-        		return 0;
-        	}
-        	else {
-        		return input;
-        	}
-        }
+    
+    void Autonomous(){
+    }
+    
+    void Test(){
+	}
+
+	double Deadband(double rawValue, double deadband) {
+		rawValue = Limit(rawValue);
+		if(Math.abs(rawValue) < deadband) return 0.0;
+		if(rawValue > 0)			  return (rawValue - deadband) / (1.0 - deadband);
+									  return (rawValue + deadband) / (1.0 - deadband);
+	}
+
+	double Limit(double value) {
+		if(value > 1.0)  return 1.0;
+		if(value < -1.0) return -1.0;
+						 return value;
+	}
 }
